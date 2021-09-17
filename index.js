d2d66a8b787b9bf8dcedd1cc0d7ff852a9e9f7d0
sidneiclementenobre <sidneiclementenobre@gmail.com>
2021-09-17 15:58:11 -0300

    enviar para github

---
+++ index.js
@@ -0,0 +1,65 @@
+var dataAtual = 4
+var dataDoEvento = 5 
+
+if(dataDoEvento >= 5) {
+    console.log("Permitir o Evento")
+}else{
+    console.log("O cadastro nao sera permitido por data invalida")
+}
+
+
+var idade = 18
+
+if(idade >= 18) {
+    console.log("Permitir o Cadastro")
+}else{ 
+    console.log("O cadastro nao foi permitido por ser menor de 18 anos")
+}
+
+
+
+
+var numero = 0
+var total = 99
+
+for(var contador = 0;contador<=total;contador++){
+    if(contador == 0){
+        console.log(" O numero e  Palestrante");
+    }else{
+        if(contador%2 == 0){ 
+            console.log("O numero " +contador+ " e Participantes");
+        }else{ 
+            if(contador%2 == 1){
+                console.log("O numero " +contador+ " e Participantes");
+            }else{ 
+
+            }
+    
+        
+
+
+
+
+
+        }
+    }            
+}    
+    
+
+
+    
+
+
+    var quantidade = 100
+
+    if(quantidade <= 100) { 
+        console.log("Permitir Cadastro quantidade menor que 100 participantes")
+    }else{
+        console.log("Nao permitir Cadastro por ter excedido o limite de 100 participantes")
+    }
+    
+           
+                
+        
+    
+ 
